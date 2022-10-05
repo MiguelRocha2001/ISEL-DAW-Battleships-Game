2b4aa3f2c39eaa3a29991dd8325ef7fe0e276c8d
MiguelRocha2001 <migasrocha1@hotmail.com>
2022-10-05 19:30:46 +0100

    Build create database file

--- code/daw-battleship-game/src/main/sql/create.sql
+++ code/daw-battleship-game/src/main/sql/create.sql
@@ -1,12 +1,64 @@
-create table BOARD (
-    id int primary key
+create table _USER(
+    id int primary key ,
+    pass_hash varchar(32)
 );
 
-create table SQUARE (
-    line int,
-    col int,
+create table PLAYER(
+    _user int primary key,
+    number int,
+    foreign key (_user) references _USER(id)
+);
+
+create table GAME(
+    id int primary key,
+    state varchar(32)
+);
+
+create table CONFIG(
+    game int primary key,
+    board_size int,
+    n_shots int,
+    timeout int,
+    foreign key (game) references GAME(id)
+);
+
+create table BOARD(
+    game int primary key,
+    is_player_1 boolean,
+    foreign key (game) references GAME(id)
+);
+
+create table PANEL(
+    game int primary key,
+    type varchar(32),
     hit boolean,
-    board_id int,
-    primary key (line, col),
-    foreign key (board_id) references BOARD(id)
+    foreign key (game) references GAME(id)
+);
+
+create table SHIP_TYPE(
+    name varchar(32) primary key
+);
+
+create table SHIPS(
+    game int,
+    type varchar(32),
+    horizontal boolean,
+    foreign key (game) references GAME(id),
+    foreign key (type) references SHIP_TYPE(name)
+);
+
+create table SHIP(
+    game int primary key,
+    type varchar(32),
+    horizontal boolean,
+    foreign key (game) references GAME(id),
+    foreign key (type) references SHIP_TYPE(name)
+);
+
+create table COORDINATE(
+    row int,
+    _column int,
+    ship int,
+    primary key (row, _column, ship),
+    foreign key (ship) references SHIP(game)
 );
