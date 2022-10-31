8fc1d2fbca13856dde6dcf63ad48c973235b2596
47185 Miguel Rocha <A47185@alunos.isel.pt>
2022-10-31 23:47:43 +0000

    Fixes connection string used in unit tests

--- code/daw-battleship-game/src/test/kotlin/pt/isel/daw/dawbattleshipgame/utils/JdbiTestUtils.kt
+++ code/daw-battleship-game/src/test/kotlin/pt/isel/daw/dawbattleshipgame/utils/JdbiTestUtils.kt
@@ -9,7 +9,7 @@ import pt.isel.daw.dawbattleshipgame.repository.jdbi.JdbiTransaction
 import pt.isel.daw.dawbattleshipgame.repository.jdbi.configure
 
 // get from env
-private val url = System.getenv("DB_POSTGRES_CONNECTION")
+private val url = System.getenv("DB_POSTGRES_BATTLESHIPS_TESTS_CONNECTION")
 
 private val jdbi = Jdbi.create(
     PGSimpleDataSource().apply {
@@ -20,21 +20,6 @@ private val jdbi = Jdbi.create(
 fun testWithHandleAndRollback(block: (Handle) -> Unit) = jdbi.useTransaction<Exception> { handle ->
     block(handle)
     handle.rollback()
-}
-
-fun testWithTransactionManager(block: (TransactionManager) -> Unit) = jdbi.useTransaction<Exception>
-{ handle ->
-
-    val transaction = JdbiTransaction(handle)
-
-    // a test TransactionManager that never commits
-    val transactionManager = object : TransactionManager {
-        override fun <R> run(block: (Transaction) -> R): R {
-            return block(transaction)
-            // n.b. no commit happens
-        }
-    }
-    block(transactionManager)
 }
 
 fun testWithTransactionManagerAndRollback(block: (TransactionManager) -> Unit) = jdbi.useTransaction<Exception>
