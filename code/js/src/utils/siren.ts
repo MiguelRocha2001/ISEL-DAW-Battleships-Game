e6dcaa5b11e7c31d4e62bfc8dd97e3cc6e3013b6
47185 Miguel Rocha <A47185@alunos.isel.pt>
2022-11-30 12:27:18 +0000

    Create siren.ts

---
+++ code/js/src/utils/siren.ts
@@ -0,0 +1,38 @@
+export type Siren = {
+    class: string;
+    properties: Object;
+    links: Link[];
+    entities: Entity[];
+    actions: Action[];
+  };
+
+type Link = {
+    rel: string[];
+    href: string;
+    title?: string;
+    type?: string;
+};
+
+type Entity = {
+    class: string[];
+    properties: Object;
+    entities: Entity[];
+    links: Link[];
+    actions: Action[];
+    title: string;
+};
+
+type Action = {
+    name: string;
+    title: string;
+    method: string;
+    href: string;
+    type: string;
+    fields: Field[];
+};
+
+type Field = {
+    name: string;
+    type: string;
+    value: string;
+};
